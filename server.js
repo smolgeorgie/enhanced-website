17628c14b4d30e546f3d09dfd29a0a4541acd22d
smolgeorgie <ilona.van.oosbree@hva.nl>
2024-04-18 21:27:06 +0200

    content

--- server.js
+++ server.js
@@ -1,40 +1,37 @@
 // Importeer het npm pakket express uit de node_modules map
 import express from 'express'
-
 // Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
 import fetchJson from './helpers/fetch-json.js'
-
 // Maak een nieuwe express app aan
 const app = express()
-
 // Stel ejs in als template engine
 app.set('view engine', 'ejs')
-
 // Stel de map met ejs templates in
 app.set('views', './views')
-
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
-
+// Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({ extended: true }))
-
-// Stel het poortnummer in waar express op moet gaan luisteren
-app.set('port', process.env.PORT || 8000)
-
-
+// Stel het basis endpoint in
 const apiUrl = 'https://fdnd-agency.directus.app/items'
-
-
-// Maak een GET route voor de index
-app.get('/', async function (request, response) {
-    // Render index.ejs uit de views map
-    const sdgData = await fetchJson(apiUrl + '/hf_sdgs')
-    response.render('index', { sdgs: sdgData.data });
-});
-
-
+const sdgData = await fetchJson(apiUrl + '/hf_sdgs')
+const stakeholdersData = await fetchJson(apiUrl + '/hf_stakeholders/1')
+const scoresData = await fetchJson(apiUrl + '/hf_scores')
+const companiesData = await fetchJson(apiUrl + '/hf_companies/1')
+console.log(companiesData.data.name)
+// Stel de routes op voor alle pagina's, de routes die hier nu in zitten gelden alleen voor de sdg en score pagina!
+app.get('/', function (request, response) {
+    response.render('index', {
+        sdgs: sdgData.data,
+        stakeholder: stakeholdersData.data,
+        score: scoresData.data,
+        company: companiesData.data
+    })
+})
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8009)
 // Start express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
     // Toon een bericht in de console en geef het poortnummer door
     console.log(`Application started on http://localhost:${app.get('port')}`)
-});
+})
