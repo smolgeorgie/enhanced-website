afd3aa71a4ff0a6cc2a767298bedfc612a8b466d
smolgeorgie <ilona.van.oosbree@hva.nl>
2024-04-19 13:21:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,17 @@ app.use(express.static('public'))
 // Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({ extended: true }))
 // Stel het basis endpoint in
+
 const apiUrl = 'https://fdnd-agency.directus.app/items'
 const sdgData = await fetchJson(apiUrl + '/hf_sdgs')
-const stakeholdersData = await fetchJson(apiUrl + '/hf_stakeholders/1')
+const stakeholdersData = await fetchJson(apiUrl + '/hf_stakeholders')
 const scoresData = await fetchJson(apiUrl + '/hf_scores')
-const companiesData = await fetchJson(apiUrl + '/hf_companies/1')
-console.log(companiesData.data.name)
+const companiesData = await fetchJson(apiUrl + '/hf_companies')
+
+console.log(companiesData.data)
+console.log(scoresData.data)
+console.log(stakeholdersData.data)
+
 // Stel de routes op voor alle pagina's, de routes die hier nu in zitten gelden alleen voor de sdg en score pagina!
 app.get('/', function (request, response) {
     response.render('index', {
@@ -28,6 +33,7 @@ app.get('/', function (request, response) {
         company: companiesData.data
     })
 })
+
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8009)
 // Start express op, haal daarbij het zojuist ingestelde poortnummer op
