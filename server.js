63871ccc152849f4028455e9d754e5ccaa7a642f
smolgeorgie <ilona.van.oosbree@hva.nl>
2024-04-23 11:31:20 +0200

    POST + footer + PE nav

--- server.js
+++ server.js
@@ -14,25 +14,35 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 // Stel het basis endpoint in
 
-// const apiUrl = 'https://fdnd-agency.directus.app/items'
-// const sdgData = await fetchJson(apiUrl + '/hf_sdgs')
-// const stakeholdersData = await fetchJson(apiUrl + '/hf_stakeholders')
-// const scoresData = await fetchJson(apiUrl + '/hf_scores')
-// const companiesData = await fetchJson(apiUrl + '/hf_companies')
+const apiUrl = 'https://fdnd-agency.directus.app/items'
+const sdgData = await fetchJson(apiUrl + '/hf_sdgs')
+const stakeholdersData = await fetchJson(apiUrl + '/hf_stakeholders')
+const scoresData = await fetchJson(apiUrl + '/hf_scores')
+const companiesData = await fetchJson(apiUrl + '/hf_companies')
 
-// console.log(companiesData.data)
-// console.log(scoresData.data)
-// console.log(stakeholdersData.data)
+console.log(scoresData.data)
 
-// // Stel de routes op voor alle pagina's, de routes die hier nu in zitten gelden alleen voor de sdg en score pagina!
-// app.get('/', function (request, response) {
-//     response.render('index', {
-//         sdgs: sdgData.data,
-//         stakeholder: stakeholdersData.data,
-//         score: scoresData.data,
-//         company: companiesData.data
-//     })
-// })
+let whatever = 75
+
+// Stel de routes op voor alle pagina's, de routes die hier nu in zitten gelden alleen voor de sdg en score pagina!
+app.get('/', function (request, response) {
+    response.render('index', {
+        sdgs: sdgData.data,
+        stakeholder: stakeholdersData.data,
+        score: scoresData.data,
+        company: companiesData.data,
+        whatever: whatever
+    })
+})
+
+app.post('/', (req, res) => {
+    if (req.body.up && whatever < 5) {
+        whatever = whatever + 1;
+    } else if (!req.body.up && whatever > -5) {
+        whatever = whatever - 1;
+    }
+    res.redirect('/');
+});
 
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8009)
