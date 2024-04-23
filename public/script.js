fb0ba0ddcbaf72090c4697d9957f4c31385bf06a
smolgeorgie <ilona.van.oosbree@hva.nl>
2024-04-23 15:30:15 +0200

    refresh code

--- public/script.js
+++ public/script.js
@@ -1,3 +1,22 @@
 document.querySelector('.menu-icon').addEventListener('click', function () {
     document.getElementById('menu-toggle').click();
 });
+
+$(document).ready(function () {
+    $('#scoreForm').submit(function (event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        $.ajax({
+            type: 'POST',
+            url: '/server.js',
+            data: $(this).serialize(), // Serialize form data
+            success: function (response) {
+                // Updates the page content with the response from the server
+                // Updates the score or any other content here
+            },
+            error: function (xhr, status, error) {
+                // Handle errors
+            }
+        });
+    });
+});
